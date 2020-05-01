9218c113aaed690a9ad285669e3c324532e01b6f
nabendu82 <nabendu.biswas@gmail.com>
2020-05-01 14:19:53 +0530

    Part-1 complete

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: `Restaurant Site`,
+    description: `The Restaurant site`,
+    author: `@thewebdev`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import { FaBlackTie } from "react-icons/fa"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+      <h3><FaBlackTie />The Restaurant Site</h3>
+  </Layout>
+)
+
+export default IndexPage
