335d0a5e8fd9c717fb981a37a16d086573bb86b8
Nabendu <nabendu.biswas@gmail.com>
2020-10-26 20:40:42 +0530

    BannerButton from home removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { HomeHeader, Banner, BannerButton } from "../utils"
+import { HomeHeader, Banner } from "../utils"
 import SEO from "../components/seo"
 import img from '../images/bcg/homeBcg.jpg'
 import QuickInfo from '../components/HomeComponents/QuickInfo'
@@ -11,9 +11,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader img={img}>
-      <Banner title="Fine Dining" subtitle="65, MG Road- Bangalore, KA">
-        <BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
-      </Banner>
+      <Banner title="Fine Dining" subtitle="65, MG Road- Bangalore, KA" />
     </HomeHeader>
     <QuickInfo />
     <Gallery />
